3d0910b89aa2171ab9d9579fec46dfa89355cbe4
Robert Buj <robert.buj@gmail.com>
2024-11-25 09:35:45 +0100

    initial commit

---
+++ frontend/src/app/Post/post.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormatDatePipe } from '../Shared/Pipes/format-date.pipe';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+
+@NgModule({
+  declarations: [
+    PostsListComponent,
+    PostFormComponent,
+    HomeComponent,
+    FormatDatePipe,
+    DashboardComponent
+  ],
+  imports: [CommonModule, ReactiveFormsModule]
+})
+export class PostModule {}
