4b81e0e13af2ee0474982bb5f1f09d26f34b9378
Robert Buj <robert.buj@gmail.com>
2024-11-28 09:37:49 +0100

    Add animation

--- frontend/src/app/Post/components/home/home.component.ts
+++ frontend/src/app/Post/components/home/home.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
@@ -11,7 +18,13 @@ import { PostService } from '../../services/post.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0.2 })),
+      transition('void <=> *', animate(1500))
+    ])
+  ]
 })
 export class HomeComponent {
   posts: PostDTO[];
